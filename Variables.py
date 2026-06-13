5d2973489200df0653037aa6b5d55759195b6267
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-13 11:01:40 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,16 @@
+#There are 5 types of Variables#
+
+a = "12"   # Its Integer
+b = "Ronaldo" # Its String
+c = "10.55" # Float
+d = "A" # Char
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Boolean
+x = 10
+y = 5
+
+print(x > y)
