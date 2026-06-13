73756cd452c3e6e9d15368a27d4283f2b5a4ddd6
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-13 13:46:43 +0500

    Add files via upload

---
+++ PP2-Assignment-1/Numbers.py
@@ -0,0 +1,8 @@
+# There are 3 types of numbers#
+x= 12 #integer
+y= 12.5 #float
+z= 1j #complex
+
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ PP2-Assignment-1/hello,world.py
@@ -0,0 +1,13 @@
+x = 10
+y = 4.5
+z ="Hello"
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Find Out Variable Type
+x = " Hello, world"
+print(x)
+"""
+
+"""
