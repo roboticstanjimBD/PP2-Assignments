b042837841a61e013fa51a70348e315648ef5b91
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-13 12:39:28 +0500

    Add files via upload

---
+++ Python Casting.py
@@ -0,0 +1,18 @@
+
+
+
+name = "S M Tanjim Rahman"
+age = 25
+gpa = 3.2
+is_student = True
+gpa = int(gpa)
+print(gpa)
+age = float(age)
+print(age)
+age = str(age)
+print(age)
+age+="1"
+print(age)
+
+name = bool(name)
+print(name)
