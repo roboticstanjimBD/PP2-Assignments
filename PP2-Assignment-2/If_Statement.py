1c4edff8a46d98478437fd076d03dcab1608d3f6
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-13 14:29:59 +0500

    Add files via upload

---
+++ PP2-Assignment-2/If_Statement.py
@@ -0,0 +1,32 @@
+'''Python supports the usual logical conditions from mathematics:
+
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b'''
+
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+'''The if statement evaluates a condition (an expression that results in True or False).
+ If the condition is true, the code block inside the if statement is executed.
+   If the condition is false, the code block is skipped.
+'''
+
+number = 15
+if number > 0:
+  print("The number is positive")
+
+#Multiple Statement
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
