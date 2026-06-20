ba856368067916f03e97f9a3922d1929def5c976
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-20 12:46:18 +0500

    Update and rename basic_lambda to basic_lambda.py

---
+++ PP@-Assignment-3/Function/lambda/basic_lambda.py
@@ -0,0 +1,34 @@
+
+"""Lambda Functions
+A lambda function is a small anonymous function.
+
+A lambda function can take any number of arguments, but can only have one expression."""
+
+
+x = lambda a : a + 10
+print(x(5))
+
+#Example
+#Multiply argument a with argument b and return the result:
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+
+#Summarize argument a, b, and c and return the result:
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+
+"""Why Use Lambda Functions?
+The power of lambda is better shown when you use them as an anonymous function inside another function.
+
+Say you have a function definition that takes one argument, and that argument will be multiplied with an unknown number:"""
+
+
+def myfunc(n):
+  return lambda a : a * n
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
