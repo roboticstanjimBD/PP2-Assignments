742e4fb7de2d50419b2ee08db096f3efb755b17b
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-20 14:12:00 +0500

    Create syntax.py

---
+++ PP2-Assignment-4/Python Iterators and Generators/json/syntax.py
@@ -0,0 +1,9 @@
+"""JSON is a syntax for storing and exchanging data.
+
+JSON is text, written with JavaScript object notation.
+
+JSON in Python
+Python has a built-in package called json, which can be used to work with JSON data."""
+
+
+import json
