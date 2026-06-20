d53d36be9932e792b6ebe8acaaff7ae705df9457
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-20 13:31:50 +0500

    Create basics.py

---
+++ PP@-Assignment-3/Function/inheritnce/basics.py
@@ -0,0 +1,33 @@
+"""Python Inheritance
+Inheritance allows us to define a class that inherits all the methods and properties from another class.
+
+Parent class is the class being inherited from, also called base class.
+
+Child class is the class that inherits from another class, also called derived class."""
+
+
+"""Create a Parent Class
+Any class can be a parent class, so the syntax is the same as creating any other class:"""
+
+"""ExampleGet your own Python Server
+Create a class named Person, with firstname and lastname properties, and a printname method:"""
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+
+"""Create a Child Class
+To create a class that inherits the functionality from another class, send the parent class as a parameter when creating the child class:"""
+
+class Student(Person):
+  pass
