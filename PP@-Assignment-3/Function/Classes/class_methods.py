8b0d3645bce763e2e5cc94b30bdd9787c9d2fc76
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-20 13:26:14 +0500

    Add files via upload

---
+++ PP@-Assignment-3/Function/Classes/class_methods.py
@@ -0,0 +1,87 @@
+"""Class Methods
+Methods are functions that belong to a class. They define the behavior of objects created from the class."""
+
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil")
+p1.greet()
+
+#Methods Modifying Properties
+#A method that changes a property value:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def celebrate_birthday(self):
+    self.age += 1
+    print(f"Happy birthday! You are now {self.age}")
+
+p1 = Person("Linus", 25)
+p1.celebrate_birthday()
+p1.celebrate_birthday()
+
+
+"""The __str__() Method
+The __str__() method is a special method that controls what is returned when the object is printed:"""
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+print(p1)
+
+
+"""Multiple Methods
+A class can have multiple methods that work together:"""
+
+class Playlist:
+  def __init__(self, name):
+    self.name = name
+    self.songs = []
+
+  def add_song(self, song):
+    self.songs.append(song)
+    print(f"Added: {song}")
+
+  def remove_song(self, song):
+    if song in self.songs:
+      self.songs.remove(song)
+      print(f"Removed: {song}")
+
+  def show_songs(self):
+    print(f"Playlist '{self.name}':")
+    for song in self.songs:
+      print(f"- {song}")
+
+my_playlist = Playlist("Favorites")
+my_playlist.add_song("Bohemian Rhapsody")
+my_playlist.add_song("Stairway to Heaven")
+my_playlist.show_songs()
+
+
+
+"""Delete Methods
+You can delete methods from a class using the del keyword:"""
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello!")
+
+p1 = Person("Emil")
+
+del Person.greet
+
+p1.greet() # This will cause an error
