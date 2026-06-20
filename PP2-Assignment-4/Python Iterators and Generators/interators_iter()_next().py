fd22bcc82ff4eca3e7122d0f3bd8b8a5c548eca6
S M Tanjim Rahman <roboticstanjim@gmail.com>
2026-06-20 13:46:28 +0500

    Create interators_iter()_next().py

---
+++ PP2-Assignment-4/Python Iterators and Generators/interators_iter()_next().py
@@ -0,0 +1,40 @@
+"""Python Iterators
+An iterator is an object that contains a countable number of values.
+
+An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+
+Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__()."""
+
+"""Iterator vs Iterable
+Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+
+All these objects have a iter() method which is used to get an iterator:"""
+
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# String Iteration
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+"""Looping Through an Iterator
+We can also use a for loop to iterate through an iterable object:"""
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
